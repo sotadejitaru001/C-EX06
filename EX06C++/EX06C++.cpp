c95efb2111588a24894d4a23720a4589307dfe59
232e001 <232e001@GAME2023PC16>
2024-01-17 14:01:26 +0900

    プロジェクト ファイルを追加します。

---
+++ EX06C++/EX06C++.cpp
@@ -0,0 +1,45 @@
+﻿using namespace std;
+
+#include <iostream>
+
+class  Box
+{
+private:
+	float width;
+	float height;
+	float depth;
+public:
+	Box(float width,float height,float depth) {
+
+		this->width = width;
+		this->height = height;
+		this->depth = depth;
+	}
+
+	float GetSurface() {
+		return(width * height + depth * height + width * height)*2;
+	}
+
+	float GetVolume() {
+		return(width*height*depth);
+	}
+};
+
+float InputFloat(string message) {
+	cout << message;
+	float i;
+	cin>>i;
+	return i;
+}
+int main() {
+	Box boxtest = Box(
+		InputFloat("幅:"),
+		InputFloat("高さ:"),
+		InputFloat("奥行:")
+	);
+	std::cout << "表面積＝" << boxtest.GetSurface()
+		<<"体積＝"<<boxtest.GetVolume()<<endl;
+	Box* boxtestnew =&boxtest ;
+	std:: cout << "表面積＝" << boxtestnew->GetSurface()
+		<< "体積＝" << boxtestnew->GetVolume() << endl;
+}
